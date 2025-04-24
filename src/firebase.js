372a644704e2e0282d3a1cac7ad1ac20322dd19f
reutnoacohen <reutnoa2000@gmail.com>
2025-04-24 13:00:24 +0300

    commit

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: <YOURAPIKEY>,
-  authDomain: <YOURAUTHDOAMIN>,
-  projectId: <YOURPROJECTID>,
-  storageBucket: <YOURSTORAGEBUCKET>,
-  messagingSenderId: <YOURMESSAGINGSENDERID>,
-  appId: <YOURAPPID>,
+  apiKey: "AIzaSyAJLeEUTRFGrMd0iYvdRQibIu0Qaw6iyRg",
+  authDomain: "pulse-anyapp.firebaseapp.com",
+  projectId: "pulse-anyapp",
+  storageBucket: "pulse-anyapp.firebasestorage.app",
+  messagingSenderId: "1032250507504",
+  appId: "1:1032250507504:web:8796d13330f6f2ae384235",
+  measurementId: "G-WY9Q5CWEBL"
 };
 
 // Initialize Firebase
